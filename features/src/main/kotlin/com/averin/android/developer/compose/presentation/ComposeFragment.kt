a4e5167311bb77c4bf3130a95ef1d5a72d71bf7b
Stas Averin <averin.developer@gmail.com>
2023-01-06 14:07:36 +0400

    Add compose tab placeholder

--- features/src/main/kotlin/com/averin/android/developer/compose/presentation/ComposeFragment.kt
+++ features/src/main/kotlin/com/averin/android/developer/compose/presentation/ComposeFragment.kt
@@ -5,8 +5,9 @@ import android.view.View
 import com.averin.android.developer.baseui.extension.androidx.fragment.app.onBackPressed
 import com.averin.android.developer.baseui.presentation.BaseViewModel
 import com.averin.android.developer.baseui.presentation.fragment.BaseFragment
+import com.averin.android.developer.dashboard.R
 
-class ComposeFragment : BaseFragment() {
+class ComposeFragment : BaseFragment(R.layout.fr_compose_placeholder) {
 
     override val viewModel: BaseViewModel? = null
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
